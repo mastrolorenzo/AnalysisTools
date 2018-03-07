f96a35af725a0f3de3401f46e636534f0291929b
mastrolorenzo <luca.mastrolorenzo@cern.ch>
2018-03-07 20:33:05 +0100

    merged fixes in HighestPtJJBJets() function + creation of HighestCMVABJets to order jets according to CMVA values

--- plugins/VHbbAnalysis.cc
+++ plugins/VHbbAnalysis.cc
@@ -391,6 +391,9 @@ bool VHbbAnalysis::Analyze() {
         j1ptCSV = m("j1ptCSV_2lepchan");
     }
 
+    std::pair<int,int> bjets_bestCMVA = HighestCMVABJets(j1ptCut, j2ptCut);
+    *in["hJetInd1_bestCMVA"] = bjets_bestCMVA.first;
+    *in["hJetInd2_bestCMVA"] = bjets_bestCMVA.second;
     std::pair<int,int> bjets_bestCSV = HighestCSVBJets(j1ptCut, j2ptCut);
     *in["hJetInd1_bestCSV"] = bjets_bestCSV.first;
     *in["hJetInd2_bestCSV"] = bjets_bestCSV.second;
@@ -402,7 +405,7 @@ bool VHbbAnalysis::Analyze() {
     *in["hJetInd2_highestPtJJ"] = bjets_highestPtJJ.second;
 
     // the jet selection algorithm we actually use for the rest of the analysis chain
-    std::pair<int,int> bjets = HighestCSVBJets(j1ptCut, j2ptCut);
+    std::pair<int,int> bjets = HighestCMVABJets(j1ptCut, j2ptCut);
 
     // put CMVA cuts out of selection functions
     if (bjets.first != -1 && bjets.second != -1) {
@@ -2347,6 +2350,43 @@ std::pair<int,int> VHbbAnalysis::HighestCSVBJets(float j1ptCut, float j2ptCut){
 
     // different pt threshold can set the highest CSV value into pair.second
     if (pair.first > -1 && pair.second > -1 && m("Jet_btagCSVV2",pair.first) < m("Jet_btagCSVV2",pair.second)) {
+        pair = std::make_pair(pair.second, pair.first);
+    }
+
+    return pair;
+}
+
+
+std::pair<int,int> VHbbAnalysis::HighestCMVABJets(float j1ptCut, float j2ptCut){
+    std::pair<int,int> pair(-1,-1);
+
+    for(int i=0; i<mInt("nJet"); i++){
+        if(mInt("Jet_puId",i) > 0
+            && m("Jet_bReg",i)>j1ptCut
+            &&fabs(m("Jet_eta",i))<=m("JetEtaCut")) {
+            if( pair.first == -1 ) {
+                pair.first = i;
+            } else if(m("Jet_btagCMVA",pair.first)<m("Jet_btagCMVA",i)){
+                pair.first = i;
+            }
+        }
+    }
+
+    for(int i=0; i<mInt("nJet"); i++){
+        if(i==pair.first) continue;
+        if(mInt("Jet_puId",i) > 0
+            && m("Jet_bReg",i)>j2ptCut
+            &&fabs(m("Jet_eta",i))<m("JetEtaCut")) {
+            if( pair.second == -1 ) {
+                pair.second = i;
+            } else if(m("Jet_btagCMVA",pair.second)<m("Jet_btagCMVA",i)){
+                pair.second = i;
+            }
+        }
+    }
+
+    // different pt threshold can set the highest CMVA value into pair.second
+    if (pair.first > -1 && pair.second > -1 && m("Jet_btagCMVA",pair.first) < m("Jet_btagCMVA",pair.second)) {
         pair = std::make_pair(pair.second, pair.first);
     }
 
