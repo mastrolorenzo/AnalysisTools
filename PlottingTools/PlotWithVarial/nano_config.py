3b3105b54aebbb2612fbbbe675b9f318637bac1b
scooperstein <stephanecooperstein@gmail.com>
2018-03-29 14:49:51 +0200

    Plot all analysis regions with Nano (#205)
    * add preliminary setup to make data/MC plots in all regions from Nano
    
    * prepare plotting samples list for 2017
    
    * add preliminary setup to make data/MC plots in all regions from Nano
    
    * first attempt at V2 2017 Z(ll) plots
    
    * add custring sample splitting
    
    * remove debug print statement
    
    * add back in CMVA plots

--- PlottingTools/PlotWithVarial/nano_config.py
+++ PlottingTools/PlotWithVarial/nano_config.py
@@ -1,12 +1,14 @@
-
+import varial
+varial.settings.rootfile_postfixes += ['.pdf']
 name = 'VHbbPlotsNanoAOD'
 #input_pattern = '/nfs/dust/cms/user/tholenhe/VHbbAnalysisNtuples/V25_VHbb_runonskim_20180121/haddjobs/sum_%s.root'
-#input_pattern = '/eos/uscms/store/user/sbc01/VHbbAnalysisNtuples/Nano2016V1_March14_v5/haddjobs/sum_%s.root'
-input_pattern = '/eos/uscms/store/group/lpchbb/VHbbAnalysisNtuples/2017V2_March22/%s/*.root'
+input_pattern = '/eos/uscms/store/group/lpchbb/VHbbAnalysisNtuples/2017V2_March27/%s/*.root'
 weight = 'weight'
 enable_reuse_step = True  # try to find output on disk and don't run a step if present
 treename = 'Events'
 
 
-from nano_samples import the_samples_dict, sample_colors
+#from nano_samples import the_samples_dict, sample_colors
+from nano_samples import *
+#from nano_samples import the_samples_dict, sample_colors
 from nano_selections import the_category_dict
--- PlottingTools/PlotWithVarial/nano_selections.py
+++ PlottingTools/PlotWithVarial/nano_selections.py
@@ -11,28 +11,28 @@ cats_all_regions = {
 }
 
 cats_SR = {
-    'SR_0Lepton':               '(controlSample==0 && isZnn==1   )',
-    'SR_1Lepton_Mu':            '(controlSample==0 && isWmunu==1 )',
-    'SR_1Lepton_El':            '(controlSample==0 && isWenu==1  )',
-    'SR_2Lepton_lowVpt_Mu':     '(controlSample==0 && isZmm==1 %s)' % low_vpt,
+#    'SR_0Lepton':               '(controlSample==0 && isZnn==1   )',
+#    'SR_1Lepton_Mu':            '(controlSample==0 && isWmunu==1 )',
+#    'SR_1Lepton_El':            '(controlSample==0 && isWenu==1  )',
+#    'SR_2Lepton_lowVpt_Mu':     '(controlSample==0 && isZmm==1 %s)' % low_vpt,
     'SR_2Lepton_highVpt_Mu':    '(controlSample==0 && isZmm==1 %s)' % high_vpt,
-    'SR_2Lepton_lowVpt_El':     '(controlSample==0 && isZee==1 %s)' % low_vpt,
-    'SR_2Lepton_highVpt_El':    '(controlSample==0 && isZee==1 %s)' % high_vpt,
+#    'SR_2Lepton_lowVpt_El':     '(controlSample==0 && isZee==1 %s)' % low_vpt,
+#    'SR_2Lepton_highVpt_El':    '(controlSample==0 && isZee==1 %s)' % high_vpt,
 }
 
 cats_CR_0Lepton = {
     'CR_0Lepton_TTbar':    '(controlSample==1 && (isWmunu==1 || isWenu == 1))',
-    'CR_0Lepton_VpLF':     '(controlSample==2 && isZnn==1)',
-    'CR_0Lepton_VpHF':     '(controlSample==3 && isZnn==1)',
+#    'CR_0Lepton_VpLF':     '(controlSample==2 && isZnn==1)',
+#    'CR_0Lepton_VpHF':     '(controlSample==3 && isZnn==1)',
 }
 
 cats_CR_1Lepton = {
     'CR_1Lepton_TTbar_Mu': '(controlSample==11 && isWmunu==1 && Vtype==2)',
-    'CR_1Lepton_VpLF_Mu':  '(controlSample==12 && isWmunu==1 && Vtype==2)',
-    'CR_1Lepton_VpHF_Mu':  '(controlSample==13 && isWmunu==1 && Vtype==2%s)' % veto_MH,
+ #   'CR_1Lepton_VpLF_Mu':  '(controlSample==12 && isWmunu==1 && Vtype==2)',
+ #   'CR_1Lepton_VpHF_Mu':  '(controlSample==13 && isWmunu==1 && Vtype==2%s)' % veto_MH,
     'CR_1Lepton_TTbar_El': '(controlSample==11 && isWenu==1 && Vtype==3%s)' % good_ele_1,
-    'CR_1Lepton_VpLF_El':  '(controlSample==12 && isWenu==1 && Vtype==3%s)' % good_ele_1,
-    'CR_1Lepton_VpHF_El':  '(controlSample==13 && isWenu==1 && Vtype==3%s%s)' % (good_ele_1, veto_MH),
+ #   'CR_1Lepton_VpLF_El':  '(controlSample==12 && isWenu==1 && Vtype==3%s)' % good_ele_1,
+ #   'CR_1Lepton_VpHF_El':  '(controlSample==13 && isWenu==1 && Vtype==3%s%s)' % (good_ele_1, veto_MH),
 }
 
 cats_CR_2Lepton_lowVpt = {
@@ -56,6 +56,10 @@ cats_CR_2Lepton_highVpt = {
 no_sel = [
     'Pass_nominal',
     'cutFlow>=2',
+    'Jet_bReg[hJetInd2] > 30',
+    'Jet_bReg[hJetInd1] > 30',
+    #'Jet_Pt[hJetInd1] > 20',
+    #'Jet_Pt[hJetInd2] > 20',
 ]
 sr_sel = no_sel + [  # copied from selection.dat
     'H_pt>100',
@@ -67,9 +71,14 @@ sr_sel = no_sel + [  # copied from selection.dat
 cs_1lep_sel = no_sel + [
     'H_pt>100',
     'V_pt>100',
-    'selLeptons_relIso_0<0.06',
-    'MET_pt<170',
+    #'selLeptons_relIso_0<0.06',
+   # 'MET_Pt<170',
 ]
+
+#cs_0lep_sel = no_sel + [
+#    #'MET_Pt>170',
+#    #'H_pt>120',
+#]
 
 # CS 2lep
 # cs_2lep_sel = no_sel + [
@@ -84,9 +93,14 @@ cs_1lep_sel = no_sel + [
 the_category_dict = {
 #   region block     categories               sel.         histograms
   #  'AllRegions':   [cats_all_regions,        no_sel,      nano_plotvariables.vars_used_in_selections],
-  #  'CR_2LeptLO':   [cats_CR_2Lepton_lowVpt,  no_sel,      nano_plotvariables.vars_2lepCR],
-    'CR_2LeptHI':   [cats_CR_2Lepton_highVpt, no_sel,      nano_plotvariables.vars_2lepCR],
-  #  'CR_1Lepton':   [cats_CR_1Lepton,         cs_1lep_sel, nano_plotvariables.vars_1lepCR],
-  #  'CR_0Lepton':   [cats_CR_0Lepton,         no_sel,      nano_plotvariables.vars_used_in_selections],
-  #  'SR':           [cats_SR,                 sr_sel,      nano_plotvariables.vars_used_in_selections],
+#    'CR_2LeptLO':   [cats_CR_2Lepton_lowVpt,  no_sel,      nano_plotvariables.test],
+#    'CR_2LeptLO':   [cats_CR_2Lepton_lowVpt,  no_sel,      nano_plotvariables.vars_2lepCR],
+#  'CR_2LeptHI':   [cats_CR_2Lepton_highVpt, no_sel,      nano_plotvariables.vars_2lepCR],
+   # 'CR_2LeptHI':   [cats_CR_2Lepton_highVpt, no_sel,      nano_plotvariables.test],
+#  'CR_1Lepton':   [cats_CR_1Lepton,         cs_1lep_sel, nano_plotvariables.vars_1lepCR],
+  'CR_1Lepton':   [cats_CR_1Lepton,         cs_1lep_sel, nano_plotvariables.test],
+#   'CR_0Lepton':   [cats_CR_0Lepton,         no_sel,      nano_plotvariables.test],
+#   'CR_0Lepton':   [cats_CR_0Lepton,         no_sel,      nano_plotvariables.vars_used_in_selections],
+ #   'SR':           [cats_SR,                 sr_sel,      nano_plotvariables.vars_used_in_selections],
+   # 'SR':           [cats_SR,                 sr_sel,      nano_plotvariables.test],
 }
