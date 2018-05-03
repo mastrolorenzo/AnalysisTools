dc34a996fc8e0a990877339fc15ed2866d5ed9bc
Sean-Jiun Wang <churchguitarist@gmail.com>
2018-05-03 14:55:44 -0500

    First pass at a restyling script for publication

---
+++ PlottingTools/PublicationStyle/restyle/utils.py
@@ -0,0 +1,24 @@
+import errno
+import os
+
+
+def safe_makedirs(path):
+    """Recursively create a directory without race conditions.
+
+    This borrows from solutions to these Stack Overflow questions:
+    * http://stackoverflow.com/a/5032238
+    * http://stackoverflow.com/a/600612
+
+    Parameters
+    ----------
+    path : path
+        The path of the created directory.
+    """
+    try:
+        os.makedirs(path)
+    except OSError as e:
+        if e.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
