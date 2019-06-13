8f2d25f77f28b4795fc3a94c0b5975a606a06e80
nhaubrich <nicholasjhaubrich@gmail.com>
2019-06-13 23:44:39 +0200

    Fixed naming error in RunAnalysis.py: site->args.site (#439)

--- VHbbAnalysis/RunAnalysis.py
+++ VHbbAnalysis/RunAnalysis.py
@@ -253,7 +253,7 @@ else:
                 # Prevent condor from transferring files just because they are new,
                 # e.g. Python bytecode, intermediate kinFit and MVA evaluation files.
                 content += "transfer_output_files = \"\"\n"
-            if site == "CERN":
+            if args.site == "CERN":
                 #Set expected job time to 24h (otherwise the default is 20 minutes)
                 content += '+JobFlavour = "tomorrow"\n'
             content += "Queue Arguments from (\n"
