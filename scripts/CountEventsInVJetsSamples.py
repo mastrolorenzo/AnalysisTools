f3c0c1187a3d892b944f85f914a31cf58ec16889
capalmer <capalmer@cern.ch>
2019-11-14 22:41:49 +0100

    Include BENR+HT for 2016 and 2018

--- scripts/CountEventsInVJetsSamples.py
+++ scripts/CountEventsInVJetsSamples.py
@@ -18,26 +18,46 @@ def PrintAllBins(hist):
             print hist.GetBinContent(iX,iY),
         print
 
+year=2016
+#if len(sys.argv)>3:
+#    year=int(sys.argv[2])
+if year==2018:
+    tuneKey="TuneCP5"
+elif year==2017:
+    tuneKey="TuneCP5"
+elif year==2016:
+    tuneKey="TuneCUETP8M1"
+else:
+    print "what year is ",year,"?"
+    sys.exit()
 
 
 listOfSamples={}
 listOfSamples[0]={}
 listOfSamples[0]["HT"]=["ZJetsToNuNu_HT-100To200_13TeV-madgraph","ZJetsToNuNu_HT-200To400_13TeV-madgraph","ZJetsToNuNu_HT-400To600_13TeV-madgraph","ZJetsToNuNu_HT-600To800_13TeV-madgraph","ZJetsToNuNu_HT-800To1200_13TeV-madgraph","ZJetsToNuNu_HT-1200To2500_13TeV-madgraph","ZJetsToNuNu_HT-2500ToInf_13TeV-madgraph"]
-listOfSamples[0]["BENR"]=["ZJetsToNuNu_BGenFilter_Zpt-100to200_TuneCP5_13TeV-madgraphMLM-pythia8_newgridpack","ZJetsToNuNu_BGenFilter_Zpt-200toInf_TuneCP5_13TeV-madgraphMLM-pythia8_newgridpack","ZBJetsToNuNu_Zpt-100to200_TuneCP5_13TeV-madgraphMLM-pythia8_newgridpack","ZBJetsToNuNu_Zpt-200toInf_TuneCP5_13TeV-madgraphMLM-pythia8_newgridpack"]
+listOfSamples[0]["BENR"]=["ZJetsToNuNu_BGenFilter_Zpt-100to200_"+tuneKey+"_13TeV-madgraphMLM-pythia8","ZJetsToNuNu_BGenFilter_Zpt-200toInf_"+tuneKey+"_13TeV-madgraphMLM-pythia8","ZBJetsToNuNu_Zpt-100to200_"+tuneKey+"_13TeV-madgraphMLM-pythia8","ZBJetsToNuNu_Zpt-200toInf_"+tuneKey+"_13TeV-madgraphMLM-pythia8"]
 
 listOfSamples[1]={}
-listOfSamples[1]["Inclu"]=["WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8"]
-listOfSamples[1]["HT"]=["WJetsToLNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8","WJetsToLNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8","WJetsToLNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8","WJetsToLNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8","WJetsToLNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8","WJetsToLNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8","WJetsToLNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8"]
-listOfSamples[1]["BENR"]=["WBJetsToLNu_Wpt-100to200_TuneCP5_13TeV-madgraphMLM-pythia8_newgridpack","WBJetsToLNu_Wpt-200toInf_TuneCP5_13TeV-madgraphMLM-pythia8_newgridpack","WJetsToLNu_BGenFilter_Wpt-100to200_TuneCP5_13TeV-madgraphMLM-pythia8_newgridpack","WJetsToLNu_BGenFilter_Wpt-200toInf_TuneCP5_13TeV-madgraphMLM-pythia8_newgridpack"]
+listOfSamples[1]["Inclu"]=["WJetsToLNu_"+tuneKey+"_13TeV-madgraphMLM-pythia8"]
+listOfSamples[1]["HT"]=["WJetsToLNu_HT-100To200_"+tuneKey+"_13TeV-madgraphMLM-pythia8","WJetsToLNu_HT-200To400_"+tuneKey+"_13TeV-madgraphMLM-pythia8","WJetsToLNu_HT-400To600_"+tuneKey+"_13TeV-madgraphMLM-pythia8","WJetsToLNu_HT-600To800_"+tuneKey+"_13TeV-madgraphMLM-pythia8","WJetsToLNu_HT-800To1200_"+tuneKey+"_13TeV-madgraphMLM-pythia8","WJetsToLNu_HT-1200To2500_"+tuneKey+"_13TeV-madgraphMLM-pythia8","WJetsToLNu_HT-2500ToInf_"+tuneKey+"_13TeV-madgraphMLM-pythia8"]
+listOfSamples[1]["BENR"]=["WBJetsToLNu_Wpt-100to200_"+tuneKey+"_13TeV-madgraphMLM-pythia8","WBJetsToLNu_Wpt-200toInf_"+tuneKey+"_13TeV-madgraphMLM-pythia8","WJetsToLNu_BGenFilter_Wpt-100to200_"+tuneKey+"_13TeV-madgraphMLM-pythia8","WJetsToLNu_BGenFilter_Wpt-200toInf_"+tuneKey+"_13TeV-madgraphMLM-pythia8"]
 
 
 
 listOfSamples[2]={}
-listOfSamples[2]["BENR"]=["DYBJetsToLL_M-50_Zpt-100to200_TuneCP5_13TeV-madgraphMLM-pythia8_newgridpack","DYBJetsToLL_M-50_Zpt-200toInf_TuneCP5_13TeV-madgraphMLM-pythia8_newgridpack","DYJetsToLL_BGenFilter_Zpt-100to200_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_newgridpack","DYJetsToLL_BGenFilter_Zpt-200toInf_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_newgridpack"]
-listOfSamples[2]["Inclu"]=["DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8"]
-listOfSamples[2]["HT"]=["DYJetsToLL_M-50_HT-70to100_TuneCP5_13TeV-madgraphMLM-pythia8","DYJetsToLL_M-50_HT-100to200_TuneCP5_13TeV-madgraphMLM-pythia8","DYJetsToLL_M-50_HT-200to400_TuneCP5_13TeV-madgraphMLM-pythia8","DYJetsToLL_M-50_HT-400to600_TuneCP5_13TeV-madgraphMLM-pythia8","DYJetsToLL_M-50_HT-600to800_TuneCP5_13TeV-madgraphMLM-pythia8","DYJetsToLL_M-50_HT-800to1200_TuneCP5_13TeV-madgraphMLM-pythia8","DYJetsToLL_M-50_HT-1200to2500_TuneCP5_13TeV-madgraphMLM-pythia8","DYJetsToLL_M-50_HT-2500toInf_TuneCP5_13TeV-madgraphMLM-pythia8"]
+if year == 2018:
+    listOfSamples[2]["BENR"]=["DYBJetsToLL_M-50_Zpt-100to200_"+tuneKey+"_13TeV-madgraphMLM-pythia8","DYBJetsToLL_M-50_Zpt-200toInf_"+tuneKey+"_13TeV-madgraphMLM-pythia8","DYJetsToLL_BGenFilter_Zpt-100to200_M-50_"+tuneKey+"_13TeV-madgraphMLM-pythia8","DYJetsToLL_BGenFilter_Zpt-200toInf_M-50_"+tuneKey+"_13TeV-madgraphMLM-pythia8"]
+    listOfSamples[2]["HT"]=["DYJetsToLL_M-50_HT-70to100_"+tuneKey+"_PSweights_13TeV-madgraphMLM-pythia8","DYJetsToLL_M-50_HT-100to200_"+tuneKey+"_PSweights_13TeV-madgraphMLM-pythia8","DYJetsToLL_M-50_HT-200to400_"+tuneKey+"_PSweights_13TeV-madgraphMLM-pythia8","DYJetsToLL_M-50_HT-400to600_"+tuneKey+"_PSweights_13TeV-madgraphMLM-pythia8","DYJetsToLL_M-50_HT-600to800_"+tuneKey+"_PSweights_13TeV-madgraphMLM-pythia8","DYJetsToLL_M-50_HT-800to1200_"+tuneKey+"_PSweights_13TeV-madgraphMLM-pythia8","DYJetsToLL_M-50_HT-1200to2500_"+tuneKey+"_PSweights_13TeV-madgraphMLM-pythia8","DYJetsToLL_M-50_HT-2500toInf_"+tuneKey+"_PSweights_13TeV-madgraphMLM-pythia8"]
+else:
+    listOfSamples[2]["BENR"]=["DYBJetsToLL_M-50_Zpt-100to200_"+tuneKey+"_13TeV-madgraphMLM-pythia8","DYBJetsToLL_M-50_Zpt-200toInf_"+tuneKey+"_13TeV-madgraphMLM-pythia8","DYJetsToLL_BGenFilter_Zpt-100to200_M-50_"+tuneKey+"_13TeV-madgraphMLM-pythia8","DYJetsToLL_BGenFilter_Zpt-200toInf_M-50_"+tuneKey+"_13TeV-madgraphMLM-pythia8"]
+    listOfSamples[2]["HT"]=["DYJetsToLL_M-50_HT-70to100_"+tuneKey+"_13TeV-madgraphMLM-pythia8","DYJetsToLL_M-50_HT-100to200_"+tuneKey+"_13TeV-madgraphMLM-pythia8","DYJetsToLL_M-50_HT-200to400_"+tuneKey+"_13TeV-madgraphMLM-pythia8","DYJetsToLL_M-50_HT-400to600_"+tuneKey+"_13TeV-madgraphMLM-pythia8","DYJetsToLL_M-50_HT-600to800_"+tuneKey+"_13TeV-madgraphMLM-pythia8","DYJetsToLL_M-50_HT-800to1200_"+tuneKey+"_13TeV-madgraphMLM-pythia8","DYJetsToLL_M-50_HT-1200to2500_"+tuneKey+"_13TeV-madgraphMLM-pythia8","DYJetsToLL_M-50_HT-2500toInf_"+tuneKey+"_13TeV-madgraphMLM-pythia8"]
 
+listOfSamples[2]["Inclu"]=["DYJetsToLL_M-50_"+tuneKey+"_13TeV-madgraphMLM-pythia8"]
 
+if year==2017:
+    for iChan in range(3):
+        for iName in range(len(listOfSamples[iChan]["BENR"])):
+            listOfSamples[iChan]["BENR"][iName]=listOfSamples[iChan]["BENR"][iName]+"_newgridpack"
 
 enrichmentCats="((LHE_Nb>0&&nGenStatus2bHad>0&&LHE_Vpt<200&&LHE_Vpt>100)*1+(LHE_Nb>0&&nGenStatus2bHad>0&&LHE_Vpt>=200)*2 +(LHE_Nb==0&&nGenStatus2bHad>0&&LHE_Vpt<200&&LHE_Vpt>100)*3+(LHE_Nb==0&&nGenStatus2bHad>0&&LHE_Vpt>=200)*4 - 1)"
 HTbins=[0,70,100,200,400,600,800,1200,2500,100000]
@@ -67,7 +87,7 @@ for chan in channels:
         hists[histName]=ROOT.TH2F(histName,";b-enrichment bin;HT bin",5,-1,4,len(HTbins)-1,HTbins_array)
         for sample in listOfSamples[chan][sampleType]:
             print sample
-            files=glob.glob(directory+"/"+sample+"/*/*/*/*root")
+            files=glob.glob(directory+"/"+sample+"/*/*/*/*.root")
             print len(files)
             histName=sample+"_BenrVsHTBin"
             hists[histName]=ROOT.TH2F(histName,";b-enrichment bin;HT bin",5,-1,4,len(HTbins)-1,HTbins_array)
